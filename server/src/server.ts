e1b201251e863c3994a67f161bd3e78585ad6d8e
victtor hugo <hgloureirocosta@gmail.com>
2020-06-04 22:32:57 -0300

    criação do banco de dados com o knex

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,9 @@
 import express from 'express';
+import path from 'path';
 import router from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(router);
-
+app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
 app.listen(3333);
